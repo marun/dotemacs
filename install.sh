7c0ba64bd1571ff5bc92ebc3b82dc9b1c0de8418
Maru Newby <marun@redhat.com>
2015-07-02 15:22:36 -0700

    Remove installation of go tools
    Better handled by something like ansible

--- install.sh
+++ install.sh
@@ -13,16 +13,8 @@ cask install
 # On OS X, homebrew pip will configure a local install path by
 # default, and passing --user on the commandline will cause an error.
 pip_cmd="pip install --upgrade"
-# For linux distros, --user is necessary to install as a non-priveleged user.
+# For linux distros, --user is necessary to install as a non-privileged user.
 if ! which sw_vers &> /dev/null ; then
     pip_cmd="$pip_cmd --user"
 fi
 $pip_cmd jedi rope flake8 importmagic
-
-go_get="go get -u"
-if which go &> /dev/null ; then
-    $go_get code.google.com/p/rog-go/exp/cmd/godef
-    $go_get github.com/dougm/goflymake
-    $go_get github.com/kisielk/errcheck
-    $go_get github.com/nsf/gocode
-fi
