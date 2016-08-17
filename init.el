72afc693f5d8820788c3578d5bf13dd62613e9cb
Maru Newby <marun@redhat.com>
2016-08-17 03:58:16 +0000

    Add package init to init.el

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;;; Compatible with Emacs 24.3 and above
 
 ;;; Cask / Pallet boilerplate
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
