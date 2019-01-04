e78e0b71efdc802044e67aaa723c030bd9a149ab
Maru Newby <marun@redhat.com>
2019-01-04 14:29:28 -0300

    Install gimme and dlv

--- install.sh
+++ install.sh
@@ -21,12 +21,20 @@ if ! which sw_vers &> /dev/null ; then
 fi
 $pip_cmd jedi rope flake8 importmagic
 
-eval $(gimme 1.11.2)
+# Install gimme
+if [ ! -f ~/bin/gimme ]; then
+  curl -sL -o ~/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/5256888df1e749fdb6acabc8f9e07a52f05b9f28/gimme
+  chmod +x ~/bin/gimme
+fi
+
+# Install golang dev tools
+eval $(~/bin/gimme 1.11.2)
 tool_urls=(
-  'github.com/rogpeppe/godef'
+  'github.com/derekparker/delve/cmd/dlv'
   'github.com/dougm/goflymake'
   'github.com/kisielk/errcheck'
   'github.com/nsf/gocode'
+  'github.com/rogpeppe/godef'
   'github.com/tools/godep'
   'golang.org/x/tools/cmd/cover'
   'golang.org/x/tools/cmd/gorename'
