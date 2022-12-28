86c776687e201a84082dd0c40d3ab262c30a3e8e
Maru Newby <me@maru.ca>
2022-12-27 21:45:45 -0800

    Temporarily disable multiple-cursors (broken)

--- init.el
+++ init.el
@@ -124,8 +124,8 @@
 
 
 ;;; Multiple cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C-c C-") 'mc/edit-lines)
+;; (require 'multiple-cursors)
+;; (global-set-key (kbd "C-c C-") 'mc/edit-lines)
 ;;(global-set-key (kbd "C->") 'mc/mark-next-like-this)
 ;;(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
 
